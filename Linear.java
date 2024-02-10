cb84bb2d474a759ae7df82a6cf8a3ec2100c43ce
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-02-10 22:13:58 +0530

    Linear Search Program using java

---
+++ Linear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Linear {
+    public static void main(String args[])    {
+        Scanner in=new Scanner(System.in);
+        int a[]={1,2,3,4,5,6,7,8,9,10};
+        System.out.print("Enter the number you wwant to search :: ");
+        int num=in.nextInt(),flag=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==num){
+                System.out.println("Number found at index number "+i);
+                flag=1;
+            }
+        }
+        if(flag==0)
+        System.out.println("Number not found");
+        in.close();
+    }
+}
