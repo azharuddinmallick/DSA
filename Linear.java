6c275d2ba75e8a8239fb89db40832c487d787281
Azhar Uddin Mallick <azharuddinmallick10@gmail.com>
2024-02-10 22:15:11 +0530

    Linear search program using java

--- Linear.java
+++ Linear.java
@@ -3,7 +3,7 @@ public class Linear {
     public static void main(String args[])    {
         Scanner in=new Scanner(System.in);
         int a[]={1,2,3,4,5,6,7,8,9,10};
-        System.out.print("Enter the number you wwant to search :: ");
+        System.out.print("Enter the number you want to search :: ");
         int num=in.nextInt(),flag=0;
         for(int i=0;i<a.length;i++){
             if(a[i]==num){
